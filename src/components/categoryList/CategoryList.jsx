ad5c1c074db82909002ebd1b5de49b7d37de3498
RizWaaN3024 <rizwaanans786@gmail.com>
2024-10-14 11:40:38 +0530

    Added components and code fixes

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const CategoryList = () => {
+// const getData = async () => {
+//   const res = await fetch("http://localhost:3000/api/categories", {
+//     cache: "no-store",
+//   });
+
+//   if (!res.ok) {
+//     throw new Error("Failed");
+//   }
+
+//   return res.json();
+// };
+
+const CategoryList = async () => {
+  // const data = await getData();
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {/* {data?.map((item) => ( */}
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.style}`}
+            key={item._id}
+          >
+              <Image
+                src={"/style.png"}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            Style
+          </Link>
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.style}`}
+            key={item._id}
+          >
+              <Image
+                src={"/fashion.png"}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            Fashion
+          </Link>
+        {/* ))} */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
