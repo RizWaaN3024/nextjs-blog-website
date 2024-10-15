88be8faac97139286e27b0b68542d67d4d963e51
RizWaaN3024 <rizwaanans786@gmail.com>
2024-10-15 17:14:34 +0530

    Code Fixes

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -3,18 +3,6 @@ import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-// const getData = async () => {
-//   const res = await fetch("http://localhost:3000/api/categories", {
-//     cache: "no-store",
-//   });
-
-//   if (!res.ok) {
-//     throw new Error("Failed");
-//   }
-
-//   return res.json();
-// };
-
 const CategoryList = async () => {
   // const data = await getData();
   return (
