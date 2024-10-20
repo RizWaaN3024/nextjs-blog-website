4814cba78fc3dc11308461757aa1c38617fde138
RizWaaN3024 <rizwaanans786@gmail.com>
2024-10-21 00:48:30 +0530

    Api integration for sigle blog page

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,53 +4,64 @@ import Image from 'next/image'
 import Menu from '@/components/menu/Menu'
 import Comments from '@/components/comments/Comments'
 
-const SinglePage = () => {
+const getData = async (slug) => {
+    const res = await fetch(
+        `http://localhost:3000/api/posts/${slug}`,
+        {
+            cache: "no-store"
+        }
+    )
+
+    if (!res.ok) {
+        throw new Error("Failed")
+    }
+
+    return res.json();
+}
+
+const SinglePage = async({ params }) => {
+    const {slug} = params;
+
+    const data = await getData(slug)
     return (
         <div className={styles.container}>
             <div className={styles.infoContainer}>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.title}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum, vero.</h1>
+                    <h1 className={styles.title}>{data?.title}</h1>
                     <div className={styles.user}>
-                        <div className={styles.userImageContainer}>
-                            <Image
-                                src={"/p1.jpeg"}
-                                alt=''
-                                fill
-                                className={styles.image}
-                            />
-                        </div>
+                        {data?.user?.image && (
+                            <div className={styles.userImageContainer}>
+                                <Image
+                                    src={data?.user.image}
+                                    alt=''
+                                    fill
+                                    className={styles.image}
+                                />
+                            </div>
+                        )}
                         <div className={styles.userTextContainer}>
-                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.username}>{data?.user.name}</span>
                             <span className={styles.date}>01.01.2024</span>
                         </div>
                     </div>
                 </div>
-                <div className={styles.imageContainer}>
-                    <Image
-                        src={"/p1.jpeg"}
-                        alt=''
-                        fill
-                        className={styles.image}
-                    />
-                </div>
+                {data?.img && (
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src={data?.img}
+                            alt=''
+                            fill
+                            className={styles.image}
+                        />
+                    </div>
+                )}
             </div>
             <div className={styles.content}>
                 <div className={styles.post}>
-                    <div className={styles.description}>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Dolorum placeat dicta aut distinctio? Voluptates tempore obcaecati facilis maxime totam 
-                            labore ipsum nulla fuga quia rerum, ullam facere voluptate quos asperiores eos nisi
-                            necessitatibus. Dignissimos a corrupti incidunt cum fugit vel.
-                        </p>
-                        <h2>Lorem ipsum dolor sit amet.</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Dolorum placeat dicta aut distinctio? Voluptates tempore obcaecati facilis maxime totam 
-                            labore ipsum nulla fuga quia rerum, ullam facere voluptate quos asperiores eos nisi
-                            necessitatibus. Dignissimos a corrupti incidunt cum fugit vel.
-                        </p>
-                    </div>
+                    <div 
+                        className={styles.description} 
+                        dangerouslySetInnerHTML={{__html: data?.desc}} 
+                    />
                     <div className={styles.comments}>
                         <Comments />
                     </div>
