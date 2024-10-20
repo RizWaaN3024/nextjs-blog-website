6056e162a91f2aa9ad9c65d4f9a357894c2dad1f
RizWaaN3024 <rizwaanans786@gmail.com>
2024-10-20 20:21:01 +0530

    APi integration for card list

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,7 +3,21 @@ import Pagination from '../pagination/Pagination'
 import styles from "./cardList.module.css"
 import Card from '../card/Card'
 
-const CardList = () => {
+const getData = async () => {
+    const res = await fetch("http://localhost:3000/api/posts", {
+        cache: "no-store",
+    })
+
+    if (!res.ok) {
+        throw new Error("Failed")
+    }
+
+    return res.json();
+}
+
+const CardList = async() => {
+
+    const data = getData();
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Recent Posts</h1>
